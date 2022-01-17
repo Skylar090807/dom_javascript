4d8d455ce7f536d3d28ec49d78e01b49db764822
Skylar <skylar090807@gmail.com>
2022-01-17 18:48:46 +0900

    Promise Study Start

---
+++ Javascript_01/promise.js
@@ -0,0 +1,8 @@
+'use strict'
+//Promise
+// point 1. State
+//:프로세스가 무거운 operation을 수행하고 있는 중인지, 수행이 완료되어 성공했는지 아니면 실패했는지 상태에 대해서 이해가 필요.
+// point 2. Producer , Consumer
+//:정보를 제공하는 Producer와 정보를 소비하는 Consumer
+//Promise is a JavaScript Object for Asynchronous Operation.
+//State: pending -> fulfilled -> rejected
