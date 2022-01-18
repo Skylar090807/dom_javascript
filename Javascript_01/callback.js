7fd95fe8878e1f2a3c1ccb002871f778f2f8d005
Skylar <skylar090807@gmail.com>
2022-01-18 12:44:50 +0900

    Synchronous Callback and Asynchronus Callback

--- Javascript_01/callback.js
+++ Javascript_01/callback.js
@@ -1,5 +1,5 @@
 'use strict'
-//Synchronous and Asynchronous 동기와 비동기
+//Synchronous and Asynchronous 동기와 비동기 ⭐️
 // Synchronous: Execute the code block in order after hoisting
 //hoisting이 된 이후 작성한 코드가 순서에 맞춰서 하나씩 동기적으로 실행.
 // Asynchronous: 작성한 코드가 언제 실행 될지 알 수 없다. 코드 작성 순서에 상관 없이 비동기적으로 실행.
@@ -33,6 +33,27 @@ console.log('3')
 //         3
 //         2
 
-//Synchronous callback
+//Callback ⭐️
 
-//Asynchronous callback
+//Synchronous callback 🟣
+function printImmediately(print) {
+  print()
+}
+printImmediately(() => console.log('hello'))
+//출력 결과: 1
+//         3
+//         hello
+//         2
+//2는 setTimeout 1초를 설정해두었으므로 1초 뒤 제일 마지막에 출력,
+//동기적으로1, 3을 출력한 뒤 hoisting된 function은 hello가 출력된다.
+
+//Asynchronous callback 🟣
+function printWithDelay(print, timeout) {
+  setTimeout(print, timeout)
+}
+printWithDelay(() => console.log('async callback'), 2000)
+//출력 결과: 1
+//         3
+//         hello
+//         2
+//         async callback
